2a1d67d666827238ab5074d0f22f9f7c8e0d051c
GOD5678 <josephklawendy2005@gmail.com>
2025-04-18 15:38:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+
+// Toggle the visibility of a dropdown menu
+const toggleDropdown = (dropdown, menu, isOpen) => {
+  dropdown.classList.toggle("open", isOpen);
+  menu.style.height = isOpen ? `${menu.scrollHeight}px` : 0;
+};
+
+// Close all open dropdowns
+const closeAllDropdowns = () => {
+  document.querySelectorAll(".dropdown-container.open").forEach((openDropdown) => {
+    toggleDropdown(openDropdown, openDropdown.querySelector(".dropdown-menu"), false);
+  });
+};
+
+// Attach click event to all dropdown toggles
+document.querySelectorAll(".dropdown-toggle").forEach((dropdownToggle) => {
+  dropdownToggle.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const dropdown = dropdownToggle.closest(".dropdown-container");
+    const menu = dropdown.querySelector(".dropdown-menu");
+    const isOpen = dropdown.classList.contains("open");
+
+    closeAllDropdowns(); // Close all open dropdowns
+    toggleDropdown(dropdown, menu, !isOpen); // Toggle current dropdown visibility
+  });
+});
+
+// Attach click event to sidebar toggle buttons
+document.querySelectorAll(".sidebar-toggler, .sidebar-menu-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    closeAllDropdowns(); // Close all open dropdowns
+    document.querySelector(".sidebar").classList.toggle("collapsed"); // Toggle collapsed class on sidebar
+  });
+});
+
+// Collapse sidebar by default on small screens
+if (window.innerWidth <= 1024) document.querySelector(".sidebar").classList.add("collapsed");
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+});
+
